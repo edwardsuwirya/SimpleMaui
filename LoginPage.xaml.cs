bca18f4e8d0c12649e5fe124b98d73fe61bdc86d
Edward Suwirya <edward_suwirya@yahoo.com>
2024-08-11 13:47:33 +0700

    Page level simple style resource, implement Localization

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FundamentalMauiApp.Resources.String;
 
 namespace FundamentalMauiApp;
 
@@ -18,25 +19,27 @@ public partial class LoginPage : ContentPage
     {
         FrameButtonSignIn.IsEnabled = false;
         IndicatorSignIn.IsVisible = true;
-        LabelSignIn.Text = "Please Wait...";
+        LabelSignIn.Text = Greetings.PleaseWait;
+        Resources["buttonPrimaryColor"] = "#9880e5";
+        Resources["textButtonSecondaryColor"] = Colors.Gray;
+        
         await Task.Delay(3000);
         var userName = EntryUserName.Text;
         var password = EntryPassword.Text;
-        
+
         if (userName == "enigma" && password == "12345")
         {
-            await DisplayAlert("Info", "Welcome", "OK");
+            await DisplayAlert(Greetings.Info, Greetings.Welcome, Greetings.Ok);
         }
         else
         {
-            await DisplayAlert("Error", "Invalid User Name or Password", "OK");
+            await DisplayAlert(Greetings.Error, Greetings.InvalidAccess, Greetings.Ok);
         }
 
         IndicatorSignIn.IsVisible = false;
-        LabelSignIn.Text = "Sign In";
+        LabelSignIn.Text = Greetings.SignIn;
         FrameButtonSignIn.IsEnabled = true;
-
-
-       
+        Resources["buttonPrimaryColor"] = "#512BD4";
+        Resources["textButtonSecondaryColor"] = "#DFD8F7";
     }
 }
